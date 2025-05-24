12483b63b0db74579549085a56e7b512cc71beac
Siema <kacper.krzyzan321@gmail.com>
2025-05-24 08:18:43 +0200

    jakoś kod do studyevent i tak to pewnei tzreba bedzi ecos porpawiac

--- symulator/src/StudyEvent.java
+++ symulator/src/StudyEvent.java
@@ -2,9 +2,13 @@ public class StudyEvent {
     private Subject subject;
     private int hours;
     private int day;
-
+    StudyEvent(Subject subject, int hours, int day) {
+        this.subject = subject;
+        this.hours = hours;
+        this.day = day;
+        subject.study(hours);
+    }
     public String getDescription() {
-        // implementacja
-        return "";
+        return subject.getName() + " " + hours + " " + day;
     }
 }
