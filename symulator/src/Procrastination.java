13d2deff2fba401ebd5be4ec0ff4cfedf795999f
bartek298 <bartosz.starkowski@wp.pl>
2025-05-26 21:39:51 +0200

    Add files via upload

--- symulator/src/Procrastination.java
+++ symulator/src/Procrastination.java
@@ -1,7 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Procrastination implements Motivator {
+    private List<Motivator> motivators;
+    private Random random = new Random();
+    public CoffeeBreak() {
+        this.motivators = new ArrayList<>();
+        motivators.add("będe mial lepsze samopoczucie");
+        motivators.add("Kasia się ze mną umowi");
+        motivators.add("bede mial szanse na lepsza prace ");
+        motivators.add("tragedia w rodzinie");
+        motivators.add("zmęczenie");
+        motivators.add("brak czasu");
+
+    }
     @Override
     public void apply(Student student) {
-        // implementacja
+        if (motivators.isEmpty()) return;
+        int index = random.nextInt(motivators.size());
+        System.out.println(motivators.get(index));
+        if (index >= 0 && index <= 2) {
+            student.setMotivation(student.getMotivation() + 1);
+        } else if (index >= 3 && index <= 5) {
+            student.setMotivation(student.getMotivation() - 1);
+        }
     }
 
     @Override
